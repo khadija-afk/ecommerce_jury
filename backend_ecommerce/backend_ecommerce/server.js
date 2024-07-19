c6033dfb90406360fc02b0e81a170f74a370ead6
khadija.kenzi <khadijaa.kenzi@gmail.com>
2024-07-19 10:31:30 +0200

    test

--- backend_ecommerce/backend_ecommerce/server.js
+++ backend_ecommerce/backend_ecommerce/server.js
@@ -21,7 +21,7 @@ import routerCartItem from './routes/cartItem.js'
 const app = express()
 
 // PORT
-const PORT = env.port || 3300
+const PORT = env.port || 9090
 import dotenv from 'dotenv';
 
 dotenv.config();
