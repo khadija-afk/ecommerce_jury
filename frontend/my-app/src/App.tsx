360fca09084e580de2ecdb4b897e3e4420285371
khadija.kenzi <khadijaa.kenzi@gmail.com>
2024-07-19 13:54:02 +0200

    Jenkinsefile

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -8,30 +8,25 @@ import ArticleList from './features/article/ArticleList';
 import AddArticleForm from './features/article/AddArticleForm';
 import AddCategoryForm from './features/category/AddCategoryForm';
 import AddReviewForm from './features/review/AddReviewForm';
-import HomePage from './pages/home'
-
-//import HomePage from './pages/home';
+import HomePage from './pages/Home';
 import Layout from './components/Layout';
 
-
 const App = () => {
-    return (
-        <Router>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<HomePage />} /> {/* Route pour la page d'accueil */}
-                        <Route path="/register" element={<RegisterForm />} />
-                        <Route path="/sign" element={<SignInForm />} />
-                        <Route path="/article" element={<ArticleList />} />
-                        <Route path="/article/add" element={<AddArticleForm />} />
-                        <Route path="/category/add" element={<AddCategoryForm />} />
-                        <Route path="/review/add" element={<AddReviewForm />} />
-                    </Route>
-                </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<HomePage />} /> {/* Route pour la page d'accueil */}
+          <Route path="register" element={<RegisterForm />} />
+          <Route path="sign" element={<SignInForm />} />
+          <Route path="article" element={<ArticleList />} />
+          <Route path="article/add" element={<AddArticleForm />} />
+          <Route path="category/add" element={<AddCategoryForm />} />
+          <Route path="review/add" element={<AddReviewForm />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
-
-
 
 export default App;
