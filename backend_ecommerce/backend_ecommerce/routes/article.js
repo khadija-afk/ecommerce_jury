1dafae668f8dbb553f37a30c411e28b0126fff2d
khadija.kenzi <khadijaa.kenzi@gmail.com>
2024-09-08 22:49:58 +0200

    zakaria add swagger

--- backend_ecommerce/backend_ecommerce/routes/article.js
+++ backend_ecommerce/backend_ecommerce/routes/article.js
@@ -13,6 +13,92 @@ import {
   getReview
 } from "../controllers/article.controller.js";
 
+/**
+ * @swagger
+ * /api/article/{id}:
+ *   get:
+ *     summary: Récupérer un élément par ID
+ *     description: Renvoie les détails d'un élément spécifique en fonction de l'ID fourni.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *           example: 1
+ *     responses:
+ *       200:
+ *         description: Détails de l'élément
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Hello, world!
+ * /api/article:
+ *   post:
+ *     summary: Renvoie un article
+ *     parameters:
+ *       - name: article_id
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Message de bienvenue
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Hello, world!
+ *   get:
+ *     summary: Renvoie un article
+ *     parameters:
+ *       - name: id
+ *         in: formData
+ *         type: integer
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Message de bienvenue
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   name: string
+ *                   example: CLIO
+ *   put:
+ *     summary: Renvoie un article
+ *     responses:
+ *       200:
+ *         description: Message de bienvenue
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Hello, world!
+ *   delete:
+ *     summary: Renvoie un article
+ *     responses:
+ *       200:
+ *         description: Message de bienvenue
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Hello, world!
+ */
 
 const router = express.Router();
 router.post('/', verifieToken, add);
