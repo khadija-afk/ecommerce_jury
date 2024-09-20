7ad0de3e3db2b73c045f52c00e7299bc134c8cb7
khadija.kenzi <khadijaa.kenzi@gmail.com>
2024-09-20 22:59:42 +0200

    fix backend

--- backend_ecommerce/backend_ecommerce/test/getById.test.js
+++ backend_ecommerce/backend_ecommerce/test/getById.test.js
@@ -1,43 +1,11 @@
 import request from 'supertest';
-import { initializeDatabase, Article, User, Categorie } from '../models/index.js';
-import createTestServer from '../serverTest.js'; // Assurez-vous que ce fichier initialise votre serveur sans démarrer l'écoute sur un port.
+
+import  '../databaseTest.js';
 
 describe('GET /api/article/:id', () => {
     let app;
     
-    beforeAll(async () => {
-        // Initialiser la base de données en mémoire et synchroniser les modèles
-        await initializeDatabase();
     
-        // Créer une instance de l'application de test
-        app = createTestServer();
-        
-        // Insérer un utilisateur de test
-        const user = await User.create({
-            firstName: 'John',
-            lastName: 'Doe',
-            email: 'john.doe@example.com',
-            password: 'password123',
-            role: 'user',
-        });
-    
-        // Insérer une catégorie de test
-        const category = await Categorie.create({
-            name: 'Test Category',
-            description: 'A category for testing',
-        });
-    
-        // Insérer des données de test avec tous les champs requis
-        await Article.create({
-            name: 'Test Article',
-            content: 'This is a test article',
-            brand: 'Test Brand', // Fournir une marque
-            price: 10.99, // Fournir un prix
-            stock: 100, // Fournir un stock
-            user_fk: user.id, // Utiliser l'ID de l'utilisateur créé
-            categorie_fk: category.id, // Utiliser l'ID de la catégorie créée
-        });
-    });
     
     
     it('devrait retourner l\'article avec l\'ID donné', async () => {
