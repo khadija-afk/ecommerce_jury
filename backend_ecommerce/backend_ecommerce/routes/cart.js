d133048ab07a94f28ca68581b57707d8a7f10a7c
khadija.kenzi <khadijaa.kenzi@gmail.com>
2024-09-26 09:12:29 +0200

    add update cart test

--- backend_ecommerce/backend_ecommerce/routes/cart.js
+++ backend_ecommerce/backend_ecommerce/routes/cart.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // Routes pour gérer les paniers avec vérification du token
 router.get('/cart', verifieToken, getCartByUserId); // Obtenir le panier d'un utilisateur connecté
 router.post('/cart', verifieToken, createCart); // Créer un panier pour un utilisateur connecté
-router.put('/cart/:userId', verifieToken, updateCartTotalAmount); // Mettre à jour le montant total du panier
+router.put('/cart', verifieToken, updateCartTotalAmount); // Mettre à jour le montant total du panier
 router.delete('/cart', verifieToken, deleteCart); // Supprimer le panier d'un utilisateur connecté
 
 export default router;
