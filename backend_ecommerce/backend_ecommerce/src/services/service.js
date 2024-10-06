8766ab21cd0c63d92a92c5ee1efa8d11f2f29e55
khadija.kenzi <khadijaa.kenzi@gmail.com>
2024-10-06 03:03:37 +0200

    fix logger Zakaria

--- backend_ecommerce/backend_ecommerce/src/services/service.js
+++ backend_ecommerce/backend_ecommerce/src/services/service.js
@@ -1,5 +1,5 @@
 
-// import logger from '../../../logger.js';
+import logger from '../../logger.js';
 
 export const destroy = async (Model, userId) => {
     let res;
@@ -48,18 +48,18 @@ export const get = async (Model, id, options = {}) => {
         model = await Model.findByPk(id, options);
 
     } catch (error) {
-        // logger.error(`Error retrieving ${Model.name}, id: ${id}, status: ${error.status}, details: ${error.message}`);
+        logger.error(`Error retrieving ${Model.name}, id: ${id}, status: ${error.status}, details: ${error.message}`);
         throw Object.assign(
             { error: 'Server error while findByPk', status: error.status || 500, details: error.message }
         );
     }
     // Si l'article n'existe pas, lancer une exception
     if (!model) {
-        // logger.warn(`${Model.name} not found id: ${id}`);
+        logger.warn(`${Model.name} not found id: ${id}`);
         throw Object.assign(
             { error: 'Not found', status: 404 }
         );
     }
-    // logger.info(`${Model.name} retrieved, id: ${id}`);
+    logger.info(`${Model.name} retrieved, id: ${id}`);
     return model; // Retourner l'article si trouvé
 };
