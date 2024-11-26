9282440663417ef7320baffa9818095a6e13f109
khadija.kenzi <khadijaa.kenzi@gmail.com>
2024-11-26 21:40:37 +0100

    update responsive header

--- frontend/my-app/src/components/header/Header.tsx
+++ frontend/my-app/src/components/header/Header.tsx
@@ -17,15 +17,15 @@ import { useAuth } from "../../utils/AuthCantext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
-  faUser,
+  faSearch,
   faHeart,
   faShoppingCart,
-  faSearch,
   faSignOutAlt,
   faUserCircle,
+  faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import MiniNavbar from "../miniNav/MiniNavbare";
-import ProfileNavBar from "../profil/profilNavbare"; // Import du ProfileNavBar
+import ProfileNavBar from "../profil/profilNavbare";
 import "./Header.css";
 
 const Header: React.FC = () => {
@@ -106,10 +106,31 @@ const Header: React.FC = () => {
 
   return (
     <header>
-      <Navbar expand="lg" bg="white" variant="light" className="py-3 navbar-custom">
-        <Container fluid>
-          <Navbar.Brand href="/">KenziShop</Navbar.Brand>
+      {/* Barre de navigation pour petits écrans */}
+      <Container fluid className="d-lg-none d-flex align-items-center justify-content-between py-2">
+        <Button
+          variant="link"
+          className="search-icon"
+          onClick={() => setShowSearchOffcanvas(true)}
+        >
+          <FontAwesomeIcon icon={faSearch} />
+        </Button>
+        <Navbar.Brand href="/" className="mx-auto text-center">
+          KenziShop
+        </Navbar.Brand>
+        <Button
+          variant="link"
+          className="menu-icon"
+          onClick={() => setShowOffcanvas(true)}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </Button>
+      </Container>
 
+      {/* Barre de navigation pour grands écrans */}
+      <Navbar expand="lg" bg="white" variant="light" className="navbar-custom py-3">
+        <Container fluid>
+          <Navbar.Brand href="/" className="d-none d-lg-inline">KenziShop</Navbar.Brand>
           <Form className="d-none d-lg-flex mx-auto search-center" onSubmit={handleSearch}>
             <FormControl
               type="search"
@@ -122,27 +143,6 @@ const Header: React.FC = () => {
               <FontAwesomeIcon icon={faSearch} />
             </Button>
           </Form>
-
-
-          {/* Icône de recherche pour petits écrans */}
-          <Button
-            variant="outline-secondary"
-            className="d-lg-none mx-auto"
-            onClick={() => setShowSearchOffcanvas(true)}
-          >
-            <FontAwesomeIcon icon={faSearch} />
-          </Button>
-
-          {/* Icône de menu pour petits écrans */}
-          <Button
-            variant="outline-secondary"
-            className="d-lg-none ms-auto"
-            onClick={() => setShowOffcanvas(true)}
-          >
-            <FontAwesomeIcon icon={faBars} />
-          </Button>
-
-          {/* Barre de navigation pour grands écrans */}
           <Nav className="d-none d-lg-flex align-items-center ms-auto">
             <Nav.Link href="/favoris" className="position-relative me-3">
               <FontAwesomeIcon icon={faHeart} />
@@ -152,7 +152,6 @@ const Header: React.FC = () => {
                 </Badge>
               )}
             </Nav.Link>
-
             <Nav.Link href="/panier" className="position-relative me-3">
               <FontAwesomeIcon icon={faShoppingCart} />
               {totalArticle > 0 && (
@@ -161,34 +160,34 @@ const Header: React.FC = () => {
                 </Badge>
               )}
             </Nav.Link>
-
             {isAuthenticated ? (
-                  <NavDropdown title={`Salut, ${userFirstName}`} id="user-dropdown">
-                    <NavDropdown.Item onClick={() => handleNavLinkClick("/profil")}>
-                      <FontAwesomeIcon icon={faUserCircle} className="me-2" />
-                      Mon compte
-                    </NavDropdown.Item>
-                    <NavDropdown.Item onClick={handleLogout}>
-                      <FontAwesomeIcon icon={faSignOutAlt} className="me-2" />
-                      Déconnexion
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                ) : (
-                  <NavDropdown title="Compte" id="user-dropdown">
-                    <NavDropdown.Item onClick={() => handleNavLinkClick("/sign")}>
-                      <FontAwesomeIcon icon={faUser} className="me-2" />
-                      Se connecter
-                    </NavDropdown.Item>
-                    <NavDropdown.Item onClick={() => handleNavLinkClick("/register")}>
-                      <FontAwesomeIcon icon={faUser} className="me-2" />
-                      Inscription
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                )}
-
+              <NavDropdown title={`Salut, ${userFirstName}`} id="user-dropdown">
+                <NavDropdown.Item onClick={() => handleNavLinkClick("/profil")}>
+                  <FontAwesomeIcon icon={faUserCircle} className="me-2" />
+                  Mon compte
+                </NavDropdown.Item>
+                <NavDropdown.Item onClick={handleLogout}>
+                  <FontAwesomeIcon icon={faSignOutAlt} className="me-2" />
+                  Déconnexion
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <NavDropdown title="Compte" id="user-dropdown">
+                <NavDropdown.Item onClick={() => handleNavLinkClick("/sign")}>
+                  <FontAwesomeIcon icon={faUser} className="me-2" />
+                  Se connecter
+                </NavDropdown.Item>
+                <NavDropdown.Item onClick={() => handleNavLinkClick("/register")}>
+                  <FontAwesomeIcon icon={faUser} className="me-2" />
+                  Inscription
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
           </Nav>
         </Container>
       </Navbar>
+
+      <div className="header-divider" />
 
       {/* Offcanvas Menu Principal */}
       <Offcanvas show={showOffcanvas} onHide={() => setShowOffcanvas(false)} placement="end">
@@ -197,7 +196,7 @@ const Header: React.FC = () => {
         </Offcanvas.Header>
         <Offcanvas.Body>
           <Nav className="d-flex flex-column align-items-start">
-            <Nav.Link onClick={() => handleNavLinkClick("/favoris")} className="position-relative mb-2">
+          <Nav.Link onClick={() => handleNavLinkClick("/favoris")} className="position-relative mb-2">
               <FontAwesomeIcon icon={faHeart} className="me-2" />
               Favoris
               {totalFavorites() > 0 && (
@@ -216,7 +215,7 @@ const Header: React.FC = () => {
                 </Badge>
               )}
             </Nav.Link>
-
+            
             {isAuthenticated ? (
               <>
                 <Nav.Link onClick={toggleProfileMenu} className="mb-2">
