b2f20eb2902db948163f7150666c1c6f8673ae60
khadija.kenzi <khadijaa.kenzi@gmail.com>
2024-11-26 22:42:07 +0100

    update responsive header

--- frontend/my-app/src/components/profil/profilNavbare.tsx
+++ frontend/my-app/src/components/profil/profilNavbare.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./profileNavebare.css";
 
 interface ProfileNavBarProps {
@@ -7,26 +8,40 @@ interface ProfileNavBarProps {
 
 const ProfileNavBar: React.FC<ProfileNavBarProps> = ({ handleNavLinkClick }) => {
   return (
-    <div className="profile-navbar">
-      <button onClick={() => handleNavLinkClick("/profil/orders")}>
-        Vos Commandes
-      </button>
-      <button onClick={() => handleNavLinkClick("/profil/security")}>
-        Connexion et Sécurité
-      </button>
-      <button onClick={() => handleNavLinkClick("/profil/adresse")}>
-        Adresse et Livraison
-      </button>
-      <button onClick={() => handleNavLinkClick("/profil/A2FGenerat")}>
-        Activer A2F
-      </button>
-      <button onClick={() => handleNavLinkClick("/profil/A2FVerefiy")}>
-        Vérifier A2F
-      </button>
-      <button onClick={() => handleNavLinkClick("/profil/A2FDesactive")}>
-        Désactiver A2F
-      </button>
-    </div>
+    <nav className="profile-navbar">
+      <ul className="profile-navbar-list">
+        <li className="profile-navbar-item">
+          <Link to="/profil/orders" onClick={() => handleNavLinkClick("/profil/orders")}>
+            Vos Commandes
+          </Link>
+        </li>
+        <li className="profile-navbar-item">
+          <Link to="/profil/security" onClick={() => handleNavLinkClick("/profil/security")}>
+            Connexion et Sécurité
+          </Link>
+        </li>
+        <li className="profile-navbar-item">
+          <Link to="/profil/adresse" onClick={() => handleNavLinkClick("/profil/adresse")}>
+            Adresse et Livraison
+          </Link>
+        </li>
+        <li className="profile-navbar-item">
+          <Link to="/profil/A2FGenerat" onClick={() => handleNavLinkClick("/profil/A2FGenerat")}>
+            Activer A2F
+          </Link>
+        </li>
+        <li className="profile-navbar-item">
+          <Link to="/profil/A2FVerefiy" onClick={() => handleNavLinkClick("/profil/A2FVerefiy")}>
+            Vérifier A2F
+          </Link>
+        </li>
+        <li className="profile-navbar-item">
+          <Link to="/profil/A2FDesactive" onClick={() => handleNavLinkClick("/profil/A2FDesactive")}>
+            Désactiver A2F
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
