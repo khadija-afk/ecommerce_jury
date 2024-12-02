28dd4e2c03583d62581a45c2394ab0c0d0280361
khadija.kenzi <khadijaa.kenzi@gmail.com>
2024-12-02 02:02:19 +0100

    add token in localstorage1

--- frontend/my-app/src/utils/axiosConfig.tsx
+++ frontend/my-app/src/utils/axiosConfig.tsx
@@ -8,12 +8,18 @@ const apiClient = axios.create({
 
 });
 
-apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-});
+);
+
 
 export default apiClient;
