1c8e7ddfbd05b92c21bbe42e6c0babd842804ceb
khadija.kenzi <khadijaa.kenzi@gmail.com>
2024-12-02 13:28:33 +0100

    vite/client

--- frontend/my-app/src/vite-env.d.ts
+++ frontend/my-app/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_PUBLIC_KEY_STRIPE: string;
+    readonly VITE_CORS_URL: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
