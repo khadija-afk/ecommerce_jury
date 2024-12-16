77f7d2403ed150caf106195cbc6c3ccf303d1400
khadija.kenzi <khadijaa.kenzi@gmail.com>
2024-12-16 11:55:07 +0100

    add interface user

--- backend_ecommerce/backend_ecommerce/src/routes/admin.js
+++ backend_ecommerce/backend_ecommerce/src/routes/admin.js
@@ -1,31 +1,43 @@
+import { sequelize } from '../models/index.js';
+import bcrypt from 'bcryptjs'; // Pour le hachage des mots de passe
 
-
-import {sequelize} from '../models/index.js'
-
-
+// Données d'utilisateur d'administration
+const ADMIN = {
+  email: 'admin@example.com',
+  password: bcrypt.hashSync('admin123', 10), // Hachez votre mot de passe pour plus de sécurité
+};
 
 // Configurer un routeur pour AdminJS
 let routerAdmin;
-console.log("routerAdmin 000");
-console.log(process.env.NODE_ENV);
 
 const initializeAdminJS = async () => {
-    // Charger AdminJS, AdminJSSequelize et AdminJSExpress de manière asynchrone
-    const AdminJS = (await import("adminjs")).default;
-    const AdminJSSequelize = (await import("@adminjs/sequelize")).default;
-    const AdminJSExpress = (await import("@adminjs/express")).default;
-  
-    // Créer une instance d'AdminJS
-    AdminJS.registerAdapter(AdminJSSequelize);
-    const adminJs = new AdminJS({
-      databases: [sequelize], // Intégration de la base de données
-    });
-  
-    console.log("AdminJS initialized successfully");
-    routerAdmin = AdminJSExpress.buildRouter(adminJs);
-  
-    
-    return routerAdmin;
-  };
+  // Charger AdminJS, AdminJSSequelize et AdminJSExpress de manière asynchrone
+  const AdminJS = (await import('adminjs')).default;
+  const AdminJSSequelize = (await import('@adminjs/sequelize')).default;
+  const AdminJSExpress = (await import('@adminjs/express')).default;
+
+  // Créer une instance d'AdminJS
+  AdminJS.registerAdapter(AdminJSSequelize);
+  const adminJs = new AdminJS({
+    databases: [sequelize], // Intégration de la base de données
+    rootPath: '/admin', // Chemin de base pour l'interface AdminJS
+  });
+
+  console.log('AdminJS initialized successfully');
+
+  // Configurer l'authentification avec AdminJSExpress
+  routerAdmin = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
+    authenticate: async (email, password) => {
+      if (email === ADMIN.email && bcrypt.compareSync(password, ADMIN.password)) {
+        return ADMIN; // Retourner l'objet utilisateur si les identifiants sont corrects
+      }
+      return null; // Retourner null si l'authentification échoue
+    },
+    cookieName: 'adminjs',
+    cookiePassword: 'some-secure-password', // Remplacez par une clé secrète
+  });
+
+  return routerAdmin;
+};
 
 export default initializeAdminJS;
